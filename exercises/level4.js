e82bd3808e2b89bd9ae7d7ff34eebc657e29e016
Josh Medeski <josh@digitalcrafts.com>
2020-02-17 14:53:06 -0600

    Add level 4 solutions

--- exercises/level4.js
+++ exercises/level4.js
@@ -1,29 +1,29 @@
-function lvl4exercise1 () {
+function lvl4exercise1() {
   // Return the boolean value "true"
-
+  return true;
 }
 
-function lvl4exercise2 () {
+function lvl4exercise2() {
   // Return the boolean value "false"
-
+  return false;
 }
 
-function lvl4exercise3 (bool) {
+function lvl4exercise3(bool) {
   // Return the opposite of the input boolean value
-
+  return !bool; // JavaScript uses "!" before a value for "not"
 }
 
-function lvl4exercise4 (bool1, bool2) {
+function lvl4exercise4(bool1, bool2) {
   // Return the logical "and" of the input boolean values
-
+  return bool1 && bool2; // JavaScript uses "&&" for "and"
 }
 
-function lvl4exercise5 (bool1, bool2) {
+function lvl4exercise5(bool1, bool2) {
   // Return the logical "or" of the input boolean values
-
+  return bool1 || bool2; // JavaScript uses "||" for "or"
 }
 
-function lvl4exercise6 (bool1, bool2) {
+function lvl4exercise6(bool1, bool2) {
   // Return the logical "equivalence" of the input boolean values
-
+  return bool1 === bool2; // Strict equivalence is using "==="
 }

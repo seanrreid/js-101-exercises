0159c640b97fffaa190e4ea03fd6b6050a0bcf83
Josh Medeski <josh@digitalcrafts.com>
2020-02-17 14:57:50 -0600

    Add level 5 solutions

--- exercises/level5.js
+++ exercises/level5.js
@@ -1,19 +1,25 @@
-function lvl5exercise1 () {
+function lvl5exercise1() {
   // Push the string "hello" into the array and return it.
-  var arr = [1, 'adam']
+  var arr = [1, "adam"];
+  arr.push("hello");
+  return arr;
 }
 
-function lvl5exercise2 () {
+function lvl5exercise2() {
   // Remove the last element from the array and return it
-  var arr = [1, 'adam']
+  var arr = [1, "adam"];
+  arr.pop();
+  return arr;
 }
 
-function lvl5exercise3 () {
+function lvl5exercise3() {
   // Return the length of the array
-  var arr = [1, 'adam']
+  var arr = [1, "adam"];
+  return arr.length;
 }
 
-function lvl5exercise4 () {
+function lvl5exercise4() {
   // Return the index of "adam" in the array
-  var arr = [1, 'adam']
+  var arr = [1, "adam"];
+  return arr.indexOf("adam");
 }

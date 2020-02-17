657a8a035d1e732e60e93f0e11b325a9ab8b913b
Josh Medeski <josh@digitalcrafts.com>
2020-02-17 14:41:42 -0600

    Add exercise 1 solutions

--- exercises/level1.js
+++ exercises/level1.js
@@ -1,34 +1,45 @@
-function lvl1exercise1 () {
+function lvl1exercise1() {
   // Declare a variable without instantiating it and return it!
-
+  // Note: This will always pass because functions return undefined when nothing is returned.
+  var variable;
+  return variable;
 }
 
-function lvl1exercise2 () {
+function lvl1exercise2() {
   // Declare and instantiate a number and return it
-
+  var number = 1;
+  return number;
 }
 
-function lvl1exercise3 () {
+function lvl1exercise3() {
   // Declare and instantiate a floating point number that is not a whole number and return it
-
+  var float = 3.14;
+  return float;
 }
 
-function lvl1exercise4 () {
+function lvl1exercise4() {
   // Declare and instantiate a string "Hello World!" and return it
-
+  var hello = "Hello World!";
+  return hello;
 }
 
-function lvl1exercise5 () {
+function lvl1exercise5() {
   // Declare and instantiate an array containing the string "Hello World!" and the number 4 and return it
-
+  var mixedArray = ["Hello World!", 4];
+  return mixedArray;
 }
 
-function lvl1exercise6 () {
+function lvl1exercise6() {
   // Declare and instantiate an object containing the key-value pairs key1 -> "Hello World!" and key2 -> 4, and return it
-
+  var mixedObject = {
+    key1: "Hello World!",
+    key2: 4
+  };
+  return mixedObject;
 }
 
-function lvl1exercise7 () {
+function lvl1exercise7() {
   // Declare and instantiate a boolean value 'false' and return it
-
+  var boolean = false;
+  return boolean;
 }

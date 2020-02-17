32ab17c851fbefbb6992f61b61e593be946bcd8c
Josh Medeski <josh@digitalcrafts.com>
2020-02-17 14:46:39 -0600

    Add solutions for level 3

--- exercises/level3.js
+++ exercises/level3.js
@@ -1,24 +1,25 @@
-function lvl3exercise1 () {
+function lvl3exercise1() {
   // Create a "hello" and a "world", return the concatenation of the two
-
+  return "hello" + "world";
 }
 
-function lvl3exercise2 () {
+function lvl3exercise2() {
   // Create a "hello" and a 12, return the concatenation of the two
-
+  return "hello" + 12;
 }
 
-function lvl3exercise3 () {
+function lvl3exercise3() {
   // Create a variable that equals 12 and convert it to a string with concatenation. Return it.
-
+  var twelve = 12;
+  return "" + twelve; // Adding an empty string to a variable with a data type will create a string
 }
 
-function lvl3exercise4 () {
+function lvl3exercise4() {
   // Create a "hello world!" string. Return the length of the string
-
+  return "hello world!".length;
 }
 
-function lvl3exercise5 () {
+function lvl3exercise5() {
   // Create a "hello world!" string. Return the index of the word "world".
-
+  return "hello world!".indexOf("world");
 }
